e45182ed27bb723f390d5d5884929b5aac41bbed
edward <edwardw@qq.com>
2017-05-05 01:46:16 +0800

    update Dockerfile

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 IMAGE="edward/freess"
+CONTAINER="freess"
 DOCKER="/usr/local/bin/docker"
-arg="-i $1"
-$DOCKER run $arg -p 1987:1987 -p 1988:1988 -p 8001:8000 $IMAGE
+arg="-i -d --rm"
+$DOCKER run $arg --name $CONTAINER -p 1987:1987 -p 1988:1988 -p 8001:8000 $IMAGE 
