986cedde28ca7f72a446231d8a50c34207fbefd3
wangwh8 <wangwh8@lenovo.com>
2017-11-13 10:06:44 +0800

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 IMAGE="edward/freess"
 CONTAINER="freess"
-DOCKER="/usr/local/bin/docker"
+DOCKER=$(which docker)
 arg="-i -d --rm"
 $DOCKER run $arg --name $CONTAINER -p 1987:1987 -p 1988:1988 -p 8001:8000 $IMAGE 
