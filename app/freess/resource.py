c90a4813068352aec7b8d95b9cffc547f595df76
edward <edwardw@qq.com>
2018-03-25 12:45:33 +0800

    tmp

--- app/freess/resource.py
+++ app/freess/resource.py
@@ -8,7 +8,7 @@ import tempfile
 
 import gevent
 import requests
-import zbarlight
+#import zbarlight
 from PIL import Image
 from gevent import socket
 from pyquery import PyQuery as pq
@@ -74,7 +74,7 @@ class FreeVPNSS(ShadowsocksResource):
 
 
 class IShadow(ShadowsocksResource):
-  url = "https://global.ishadowx.net/"
+  url = "https://en.ishadowx.net/"
   #proxy = 'http://127.0.0.1:64548'
 
   def iter_params(self, response):
